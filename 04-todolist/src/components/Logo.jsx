404cc4bc2401ab8f43e777c084b743f68e34cef9
woolen-h <woolen_h@kakao.com>
2024-08-17 18:00:43 +0900

    feat: Logo 컴포넌트 생성

---
+++ 04-todolist/src/components/Logo.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Logo() {
+  return (
+    <div className="LogoContainer">
+      <img src="../../public/Logo.svg" alt="Logo" />
+    </div>
+  );
+}
+
+export default Logo;

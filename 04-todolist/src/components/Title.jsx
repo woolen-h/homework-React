dc9e2b820931f9d34fa2bcdac950d6ab6b125281
woolen-h <woolen_h@kakao.com>
2024-08-17 18:01:05 +0900

    feat: Title 컴포넌트 생성

---
+++ 04-todolist/src/components/Title.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Title() {
+  return (
+    <>
+      <h1 className="Title__Head">우리, 오늘 뭐할까?</h1>
+      <p className="Title__Date">날짜(요일)</p>
+    </>
+  );
+}
+
+export default Title;

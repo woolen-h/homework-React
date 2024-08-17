9e661bfc4d2f48fc8fa5af9198a99b1473735eff
woolen-h <woolen_h@kakao.com>
2024-08-17 22:50:17 +0900

    feat: todo 컴포넌트 업데이트

--- 04-todolist/src/components/Todo.jsx
+++ 04-todolist/src/components/Todo.jsx
@@ -1,10 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Todo() {
+function Todo({ title, description, time, isCompleted, onArchive }) {
+  const [checked, setChecked] = useState(isCompleted);
+
   return (
-    <ul className="todo__container">
-      <li>투두리스트</li>
-    </ul>
+    <div className="todo">
+      <ul className="todo__container">
+        <li>
+          <h1 className="todo__title">
+            <span>{title}</span>
+            <span>
+              <input
+                type="checkbox"
+                checked={checked}
+                onChange={() => setChecked(!checked)}
+              />
+            </span>
+          </h1>
+          <p className="todo__disc">{description}</p>
+          <p className="divider"></p>
+          <p className="todo__time">
+            <span>{time}</span>
+            <button
+              className="button__archive"
+              type="button"
+              onClick={onArchive}
+            >
+              <span className="icon__archive"></span>
+            </button>
+          </p>
+        </li>
+      </ul>
+    </div>
   );
 }
 

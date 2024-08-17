78ca962f8e7e5d8eb3a99494c7a5aea55cb796e0
woolen-h <woolen_h@kakao.com>
2024-08-17 17:59:58 +0900

    feat: button 컴포넌트 생성

---
+++ 04-todolist/src/components/AddButton.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function AddButton() {
+  return (
+    <div className="button__container">
+      <button className="button__primary" type="button">
+        <span className="icon__plus">+</span>생각났어?
+      </button>
+    </div>
+  );
+}
+
+export default AddButton;

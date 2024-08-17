bb044411b62a5b0590293fd8bc24a7c051584e23
woolen-h <woolen_h@kakao.com>
2024-08-17 17:58:48 +0900

    feat: 템플릿 생성 및 todolist 환경 설정

---
+++ 04-todolist/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Layout from "./layout/Layout";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Assignment : 오늘 할 일(Today Do It)</h1>
+      <Layout />
+    </div>
+  );
+}
+
+export default App;

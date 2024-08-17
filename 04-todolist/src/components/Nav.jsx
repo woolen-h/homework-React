6cc7c697af634db64dc16f7fb521c0b7f421af05
woolen-h <woolen_h@kakao.com>
2024-08-17 18:00:53 +0900

    feat: Nav 컴포넌트 생성

---
+++ 04-todolist/src/components/Nav.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Nav() {
+  return (
+    <ul className="nav">
+      <li>
+        모두<span>1</span>
+      </li>
+      <li>
+        할일<span>1</span>
+      </li>
+      <li>
+        한일<span>1</span>
+      </li>
+      <li>
+        보관<span>1</span>
+      </li>
+    </ul>
+  );
+}
+
+export default Nav;
